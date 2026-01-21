c52eaca349babd3c8c5edb326284615fbd10106e
loaf-of-bread52 <mstewart2029@csw.org>
2026-01-21 16:03:37 -0500

    Hopper & Intake Code

---
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Hopper {
+
+
+    // make motors visible to class
+    SparkMax top_motor;
+    SparkMax bot_motor;
+
+    // set max speed
+    double topMaxSpeed = 1;
+    double botMaxSpeed = 1;
+
+    // make motor
+    public Hopper(int t_id, int b_id){
+        top_motor = new SparkMax(t_id, MotorType.kBrushless);
+        bot_motor = new SparkMax(b_id, MotorType.kBrushless);
+    }
+
+    // sets the motors which makes the motors set the motors to set the motors which to sets the motors to set the motors to set the motors to set the speed of the motors
+    public void SetMotors(double sp1, double sp2){
+        top_motor.set(Math.min(sp1, topMaxSpeed));
+        bot_motor.set(Math.min(sp2, botMaxSpeed));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Intake {
+    
+    SparkMax m_motor;
+    SparkMax ex_motor;
+
+    double mMaxSpeed = 1;
+    double exMaxSpeed = 1;
+
+    double nowAngle = 0;
+    double plsAngle;
+    double maxAngle = 180;
+
+    public Intake(int m_id, int ex_id){
+        m_motor = new SparkMax(m_id, MotorType.kBrushless);
+        ex_motor = new SparkMax(ex_id, MotorType.kBrushless);
+    }
+
+    public void StartIntake(double mSpeed){
+        m_motor.set(Math.min(mMaxSpeed, mSpeed));
+    }
+
+    public void ExtendIntake(double angle){
+        plsAngle = angle;
+    }
+
+    public void robotPeriodic(){
+        if (nowAngle >= 180 || nowAngle <= 180){
+            ex_motor.set(0);
+        } else {
+            if (plsAngle > nowAngle){
+                ex_motor.set(Math.min(-maxAngle / (Math.abs(plsAngle - nowAngle)), exMaxSpeed));
+            } else {
+                ex_motor.set(Math.min(maxAngle / (Math.abs(plsAngle - nowAngle)), exMaxSpeed));
+            }
+        }
+    }
+
+}
