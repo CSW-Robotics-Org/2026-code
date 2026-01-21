fa5bfbcf524a46de1e4e4cee11d5bca2e32d446b
atbutler09 <atbutler09@outlook.com>
2026-01-21 15:50:07 -0500

    hopper code written by andrzej

---
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+public class Hopper {
+    // Vars:
+    /*
+     * Motor 1, motor2, maxspeed
+     */
+    // Methods;
+    /*
+     * Constructor
+     * Set motors
+     */
+    
+    private SparkMax motor_1;
+    private SparkMax motor_2;
+    public double mMaxSpeed = 1;
+   
+    public Hopper (int m1_id, int m2_id) {
+        motor_1 = new SparkMax(m1_id, MotorType.kBrushless);
+        motor_2 = new SparkMax(m2_id, MotorType.kBrushless);
+    }
+
+    // Command for Shooter Speed
+    public void MotorShoot() {
+        
+    }
+
+}
