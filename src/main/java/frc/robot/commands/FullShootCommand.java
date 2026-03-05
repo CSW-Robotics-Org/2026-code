7f70b3c9f4e0cd723b3c2b44a406094152a61591
Nicholas Palmer <npalmer2028@csw.org>
2026-03-05 16:12:09 -0500

    Pulsing?

--- src/main/java/frc/robot/commands/FullShootCommand.java
+++ src/main/java/frc/robot/commands/FullShootCommand.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
@@ -26,7 +27,10 @@ public class FullShootCommand extends Command {
 
     private final TurretRotationCommand rotationCommand;
     private final TurretPowerCommand powerCommand;
-    
+
+    private Timer pulseTimer = new Timer();
+    private boolean feederOn = false;
+        
 
 
 
@@ -46,20 +50,45 @@ public class FullShootCommand extends Command {
 
         rotationCommand = new TurretRotationCommand(turret, limelight, drivetrain);
         powerCommand = new TurretPowerCommand(turret, limelight, drivetrain);
+
+        pulseTimer.reset();
     }
 
     @Override
     public void execute() {
+        pulseTimer.start();
         rotationCommand.execute();
         powerCommand.execute();
         System.out.println("turret ready? : " + rotationCommand.rotationReady() + ", Angle Error: " + rotationCommand.angleError);
         System.out.println("shooter ready? : " + powerCommand.readyToShoot() + ", CurrentRPM, targetRPM: " + turret.s_encoder.getVelocity() + ", " + turret.targetRPM);
 
-        if (rotationCommand.rotationReady() && powerCommand.readyToShoot()){
-            turret.setFeederMotor(0.5);
+        if (rotationCommand.rotationReady() && powerCommand.readyToShoot()) {
+
             hopper.setHopperMotor(0.5);
             intake.setIntakeMotor(0.3);
-        }
+
+                double time = pulseTimer.get();
+
+                if (feederOn) {
+                    turret.setFeederMotor(0.5);
+
+                    if (time > 0.1) { // after 0.1 sec turn off
+                        feederOn = false;
+                        pulseTimer.reset();
+                    }
+
+                } else {
+                    turret.setFeederMotor(0);
+                    
+                    if (time > 0.1) { // wait 0.1 sec then turn on again
+                        feederOn = true;
+                        pulseTimer.reset();
+                    }
+                }
+
+            }
+
+
 
     }
 
@@ -70,6 +99,8 @@ public class FullShootCommand extends Command {
         hopper.setHopperMotor(0);
         turret.setFeederMotor(0);
         intake.setIntakeMotor(0);
+        pulseTimer.stop();
+        pulseTimer.reset();
     }
 
     @Override
